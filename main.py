e9e00d17e789e2745392da537ba341e0fe1af74b
░▒▓█│【Walkman】│█▓▒░ <matthewcarter17@gmail.com>
2016-02-24 21:39:35 +0200

    added roman numeral converters using NumeralConverter

--- main.py
+++ main.py
@@ -170,6 +170,109 @@ class WebhookHandler(webapp2.RequestHandler):
                 return True
             return False
         
+        def is_number(s):
+            try:
+                float(s)
+                return True
+            except ValueError:
+                return False
+        # https://github.com/Walkman100/NumeralConverter/blob/17fe334fb34088fbd7c7c25ef31fba5df8961512/Python27/numeralconverter.py#L68
+        def checkAndOutputRomanNumeral(input):
+            if is_number(input):
+                if len(input) < 19:
+                    outputRomanNumeral(int(input))
+                else:
+                    reply("\"" + input + "\" is " + (len(input) - 18 + " digit(s) too long! Maximum size for \"//\" operations is 18 digits"))
+            else:
+                reply("\"" + input + "\" is not an Arabic number!")
+        def outputRomanNumeral(number):
+            returnString = ""
+            if number > 1000:
+                for i in range(1, (number // 1000) +1):
+                    returnString = returnString + "M"
+                number = number - (number // 1000) * 1000
+            while number > 900:
+                number = number - 900
+                returnString = returnString + "CM"
+            
+            while number > 500:
+                number = number - 500
+                returnString = returnString + "D"
+            while number > 400:
+                number = number - 400
+                returnString = returnString + "CD"
+            
+            while number > 100:
+                number = number - 100
+                returnString = returnString + "C"
+            while number > 90:
+                number = number - 90
+                returnString = returnString + "XC"
+            
+            while number > 50:
+                number = number - 50
+                returnString = returnString + "L"
+            while number > 40:
+                number = number - 40
+                returnString = returnString + "XL"
+            
+            while number > 10:
+                number = number - 10
+                returnString = returnString + "X"
+            while number > 9:
+                number = number - 9
+                returnString = returnString + "IX"
+            
+            while number > 5:
+                number = number - 5
+                returnString = returnString + "V"
+            while number > 4:
+                number = number - 4
+                returnString = returnString + "IV"
+            
+            while number >= 1:
+                number = number - 1
+                returnString = returnString + "I"
+            reply(returnString)
+        def outputArabicNumber(RomanNumber):
+            RomanNumber = RomanNumber.upper()
+            nonvalid = ""
+            for i in range(0, len(RomanNumber)):
+                # https://stackoverflow.com/a/1228327/2999220
+                if RomanNumber[i] == "I":   RomanNumber = RomanNumber[:i] + '1' + RomanNumber[i + 1:]
+                elif RomanNumber[i] == "V": RomanNumber = RomanNumber[:i] + '2' + RomanNumber[i + 1:]
+                elif RomanNumber[i] == "X": RomanNumber = RomanNumber[:i] + '3' + RomanNumber[i + 1:]
+                elif RomanNumber[i] == "L": RomanNumber = RomanNumber[:i] + '4' + RomanNumber[i + 1:]
+                elif RomanNumber[i] == "C": RomanNumber = RomanNumber[:i] + '5' + RomanNumber[i + 1:]
+                elif RomanNumber[i] == "D": RomanNumber = RomanNumber[:i] + '6' + RomanNumber[i + 1:]
+                elif RomanNumber[i] == "M": RomanNumber = RomanNumber[:i] + '7' + RomanNumber[i + 1:]
+                else:
+                    nonvalid = RomanNumber[i]
+            if nonvalid:
+                reply("\"" + nonvalid + "\" is not a valid Roman Numeral character!")
+            else:
+                # Now we have the roman number in arabic numbers (so we can use < and >), we just add it all
+                ArabicNumber = 0
+                RomanNumber = RomanNumber + "0" # Because loops, length calculation and next letter calculation
+                for i in range(0, len(RomanNumber)):
+                    if i < len(RomanNumber) - 1 and RomanNumber[i] >= RomanNumber[i + 1]:
+                        if RomanNumber[i] == "1":   ArabicNumber = ArabicNumber + 1
+                        elif RomanNumber[i] == "2": ArabicNumber = ArabicNumber + 5
+                        elif RomanNumber[i] == "3": ArabicNumber = ArabicNumber + 10
+                        elif RomanNumber[i] == "4": ArabicNumber = ArabicNumber + 50
+                        elif RomanNumber[i] == "5": ArabicNumber = ArabicNumber + 100
+                        elif RomanNumber[i] == "6": ArabicNumber = ArabicNumber + 500
+                        elif RomanNumber[i] == "7": ArabicNumber = ArabicNumber + 1000
+                    elif i < len(RomanNumber) - 1:
+                        if RomanNumber[i] == "1":   ArabicNumber = ArabicNumber - 1
+                        elif RomanNumber[i] == "2": ArabicNumber = ArabicNumber - 5
+                        elif RomanNumber[i] == "3": ArabicNumber = ArabicNumber - 10
+                        elif RomanNumber[i] == "4": ArabicNumber = ArabicNumber - 50
+                        elif RomanNumber[i] == "5": ArabicNumber = ArabicNumber - 100
+                        elif RomanNumber[i] == "6": ArabicNumber = ArabicNumber - 500
+                        elif RomanNumber[i] == "7": ArabicNumber = ArabicNumber - 1000
+                reply(str(ArabicNumber))
+        
         # COMMANDS BELOW
         
         if text.endswith('@WalkmanBot'): text = text[:-11]
@@ -205,6 +308,8 @@ class WebhookHandler(webapp2.RequestHandler):
                     helpText = helpText + '\n/shout <text> - Shout <text> in caps'
                     helpText = helpText + '\n/image - Send a randomly generated image'
                     helpText = helpText + '\n/curl <url> - Return the text of <url> (Warning: reply could be very long!)'
+                    helpText = helpText + '\n/r2a <roman numerals> - Convert Roman Numerals to Arabic numbers'
+                    helpText = helpText + '\n/a2r <arabic number> - Convert Arabic numbers to Roman Numerals'
                     # helpText = helpText + '\n/'
                     send_message(helpText)
                 elif text.lower() == '/image':
@@ -269,6 +374,20 @@ class WebhookHandler(webapp2.RequestHandler):
                     except ValueError, err:
                         logging.info('ERROR: ' + str(err))
                         reply('ERROR: ' + str(err))
+                elif text.lower() == '/r2a':
+                    reply('Usage: /r2a <roman numerals>')
+                elif text.lower().startswith('/r2a'):
+                    text = text[4:]
+                    if text.startswith('@WalkmanBot'): text = text[11:]
+                    if text.startswith(' '): text = text[1:]
+                    outputArabicNumber(text)
+                elif text.lower() == '/a2r':
+                    reply('Usage: /a2r <arabic number>')
+                elif text.lower().startswith('/a2r'):
+                    text = text[4:]
+                    if text.startswith('@WalkmanBot'): text = text[11:]
+                    if text.startswith(' '): text = text[1:]
+                    checkAndOutputRomanNumeral(text)
                 
                 else:
                     if getUnknownCommandEnabled(chat_id):
