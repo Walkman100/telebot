8d296cb0ef648e6abe6b3614b939717a1a704ded
░▒▓█│【Walkman】│█▓▒░ <matthewcarter17@gmail.com>
2016-06-17 00:45:42 +0200

    Added Linux upload.sh, added quotation marks to Windows upload.bat

--- upload.bat
+++ upload.bat
@@ -1,6 +1,6 @@
 @echo off
 python2 main.py
 echo.
-echo If output says "ImportError: No module named PIL" then press enter
+echo If output says "ImportError: No module named PIL" then press enter...
 pause
-python2 -u %ProgramFiles%\Google\AppEngine\appcfg.py --oauth2_credential_file=%UserProfile%\.appcfg_oauth2_tokens update %~dp0
+python2 -u "%ProgramFiles%\Google\AppEngine\appcfg.py" --oauth2_credential_file="%UserProfile%\.appcfg_oauth2_tokens" update "%~dp0"
