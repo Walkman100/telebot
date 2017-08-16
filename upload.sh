333afc8e978dc3615f19b9f62de780dd36d4dcb6
░▒▓█│【Walkman】│█▓▒░ <matthewcarter17@gmail.com>
2017-08-16 20:34:39 +0200

    improved upload.sh to quit if enter isn't pressed

--- upload.sh
+++ upload.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 python2.7 main.py
 echo
-read -n1 -r -p "If output says \"ImportError: No module named appengine.api\" then press enter... "
-python2.7 -u "$HOME/bin/google_appengine/appcfg.py" --oauth2_credential_file="$HOME/.appcfg_oauth2_tokens" update "$PWD"
+read -n1 -r -p "If output says \"ImportError: No module named appengine.api\" then press Enter or Space... " CONDITION;
+echo
+
+if [ "$CONDITION" == "" ]; then
+   python2.7 -u "$HOME/bin/google_appengine/appcfg.py" --oauth2_credential_file="$HOME/.appcfg_oauth2_tokens" update "$PWD"
+fi
